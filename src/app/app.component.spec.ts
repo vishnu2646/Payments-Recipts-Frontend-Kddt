76337dd202cd85bc510187d1bb9ab6a88803f625
vishnupriyan <priyanvishnu15@gmail.com>
2024-12-15 12:05:41 +0530

    added fontend for payments & recipts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'kdda' title`, () => {
+  it(`should have the 'kddt' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kdda');
+    expect(app.title).toEqual('kddt');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kdda');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kddt');
   });
 });
