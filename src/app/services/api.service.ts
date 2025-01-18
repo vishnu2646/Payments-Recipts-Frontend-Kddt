8c5f32339ebdd62cbd971a17e17c56c25540ddc4
vishnupriyan <priyanvishnu15@gmail.com>
2025-01-18 11:30:29 +0530

    updated url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,9 +8,9 @@ import { IExepnse, IExpenseType, IIncome, IIncomeType } from '../types/types';
 })
 export class ApiService {
 
-    private baseUrl = 'http://127.0.0.1:8000/api/user';
+    // private baseUrl = 'http://127.0.0.1:8000/api/user';
 
-    // private baseUrl = 'https://kddt.pythonanywhere.com/api/user';
+    private baseUrl = 'https://kddt.pythonanywhere.com/api/user';
 
     private httpClient = inject(HttpClient);
 
