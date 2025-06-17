fa0e2733828cf6a9304ef2ef97a2c986dc71c034
vishnupriyan <priyanvishnu15@gmail.com>
2025-06-17 16:29:55 +0530

    fix: updates the url in env

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://kdda.pythonanywhere.com/api/user',
+    apiUrl: 'https://kddt.pythonanywhere.com/api/user',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    apiUrl: 'https://kddt.pythonanywhere.com/api/user',
+};
